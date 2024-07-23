51c8e57708ef600c18f4b4db5121bbdc8ea744b2
Satendra <satendrasahani20@gmail.com>
2024-07-23 10:47:15 +0530

    change design

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -6,42 +6,42 @@ const Footer = () => {
  <div className=" col-md-3 footer-container-1 text-center">
   <h3>Company</h3>
   
-  <p className="mt-2 ml-1">
+  <div className="mt-2 ml-1">
   <p>About Us</p>
   <p>Our Servicse</p>
   <p>Privacy Policy</p>
   <p>Affiliate Program</p>
-  </p >
+  </div >
  </div>
 
  <div className=" col-md-3 footer-container-1  text-center">
  <h3>Get Help</h3>
- <p className="mt-2 ml-1">
+ <div className="mt-2 ml-1">
  <p>FAQ</p>
  <p>Shipping</p>
  <p>Returns</p>
  <p>Order Status</p>
  <p>Payment Options</p>
- </p>
+ </div>
  </div>
 
  <div className=" col-md-3 footer-container-1  text-center">
  <h3>Online Shop</h3>
- <p className="mt-2 ml-1">
+ <div className="mt-2 ml-1">
  <p>Watch</p>
  <p>Bag</p>
  <p>Shose</p>
  <p>Dress</p>
- </p>
+ </div>
  </div>
 
  <div className=" col-md-3 footer-container-1  text-center">
  <h3>Follow Us</h3>
  <div className="mt-2 ">
- <div className="arrow-1"><i class="fa fa-twitter"></i></div>
-<div className="arrow-1"><i class="fa fa-facebook"></i></div>
-<div className="arrow-1"><i class="fa fa-instagram"></i></div>
-<div className="arrow-1"><i class="fa fa-behance"></i></div>
+ <div className="arrow-1"><i className="fa fa-twitter"></i></div>
+<div className="arrow-1"><i className="fa fa-facebook"></i></div>
+<div className="arrow-1"><i className="fa fa-instagram"></i></div>
+<div className="arrow-1"><i className="fa fa-behance"></i></div>
 </div>
  </div>
   </footer>
